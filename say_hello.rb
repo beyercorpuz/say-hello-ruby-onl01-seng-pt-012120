3da9707e67a6e4ce4564c0de4b389e3ae360fde3
Chris Beyer <beyercorpuz@gmail.com>
2020-01-22 21:56:54 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
 # I call on the method, say_hello, and give it the string "Gabriela" 
-say_hello("Gabriela",`"Hello Ruby Programmer!"`)learn test
+say_hello("Gabriela",`"Hello Ruby Programmer!"`)learn 
  
 # The method prints this text to the screen:
 Hello Gabriela!
